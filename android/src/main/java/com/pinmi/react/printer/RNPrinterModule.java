14100388625a1e74e07a21105d170bb451a321fd
savanitdev@gmail.com <savanitdev@mail.comgit config --global user.email savanitdev@gmail.com>
2024-01-31 11:39:28 +0700

    init

---
+++ android/src/main/java/com/pinmi/react/printer/RNPrinterModule.java
@@ -0,0 +1,29 @@
+package com.pinmi.react.printer;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+/**
+ * Created by xiesubin on 2017/9/21.
+ */
+
+public interface RNPrinterModule {
+
+    public void init(Callback successCallback, Callback errorCallback);
+
+    public void closeConn();
+
+    public void getDeviceList(Callback successCallback, Callback errorCallback);
+
+    @ReactMethod
+    public void printRawData(String base64Data, Callback errorCallback);
+    
+    @ReactMethod
+    public void printRawData2(String base64Data, Promise promise);
+
+    @ReactMethod
+    public void printImageData(String imageUrl, int imageWidth, int imageHeight, Callback errorCallback);
+
+    @ReactMethod
+    public void printImageBase64(String base64, int imageWidth, int imageHeight, Callback errorCallback) ;
+}
+
